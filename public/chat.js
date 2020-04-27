1fa8fe54206f75da327cd5b79386d472171f21d2
umitrecepogut <ogutumitrecep@gmail.com>
2020-04-27 14:03:02 +0300

    Styles Changed

--- public/chat.js
+++ public/chat.js
@@ -1,15 +1,11 @@
-//Make Connection
 var socket = io.connect('http://localhost:4000');
 
-
-// Query DOM
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
       output = document.getElementById('output'),
       feedback = document.getElementById('feedback');
 
-// Emit Events
 btn.addEventListener('click', function(){
     socket.emit('chat', {
         message: message.value,
@@ -22,7 +18,6 @@ message.addEventListener('keypress', function(){
     socket.emit('typing', handle.value);
 })
 
-// Listen for Events
 socket.on('chat', function(data){
     feedback.innerHTML = '';
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
